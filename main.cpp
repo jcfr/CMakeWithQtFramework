277be7a47f8296abdd361952abba732162e4c6bc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-11-29 18:07:24 -0500

    Add WebKit dependency

--- main.cpp
+++ main.cpp
@@ -2,15 +2,27 @@
 
 #include <QApplication>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QWebView>
 
 int main(int argc, char*argv[])
 {
   QApplication app(argc, argv);
   
-  QPushButton button("Quit");
-  QObject::connect(&button, SIGNAL(clicked()), &app, SLOT(quit()));
+  QWidget topLevel;
+  QVBoxLayout * layout = new QVBoxLayout();
+  topLevel.setLayout(layout);
 
-  button.show(); 
+  QPushButton * button = new QPushButton("Quit");
+  layout->addWidget(button);
+  
+  QWebView * webView = new QWebView();
+  webView->setUrl(QUrl("http://github.com"));
+  layout->addWidget(webView);
+  
+  QObject::connect(button, SIGNAL(clicked()), &app, SLOT(quit()));
+
+  topLevel.show();
   
   return app.exec();
 }
